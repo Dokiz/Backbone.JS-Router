a0133eead25add4f410ce82e44f321713f2464e0
Dokiz_228@mail.ru <deoptgqqr12>
2016-12-10 08:37:39 +0300

    start

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 var app = app || {};
-$ (function(){
-	app.router = new Router();
-}); 
+$(function () {
+    app.router = new Router();
+    $('#search').on('click',function(){
+        var query = $('#query').val();
+        var category = $('#category').val();
+        app.router.navigate("first/"+query+"/"+category, {trigger: true});
+        return false;
+    });
+});
